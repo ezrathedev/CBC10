4098f156c096ca9a0ef1733b2e52bd8c930c6ac2
unknown <anin4325@gmail.com>
2020-10-15 19:57:39 -0700

    update

--- week4/app3.js
+++ week4/app3.js
@@ -17,28 +17,30 @@ console.log('bot2: ',bot2);
 var result = '';
 if (bot1 === choices[0] & bot2 === choices[0]){
     console.log('tied game both pick rock'); // both choose rock
-} else if (bot1 === choices[1] &  bot2 === choices[1]) {
+} else if (bot1 === choices[1] &&  bot2 === choices[1]) {
     console.log('tied game both pick paper'); // both choose paper
-} else if(bot1 === choices[2] &  bot2 === choices[2]) {
+} else if(bot1 === choices[2] &&bot2 === choices[2]) {
     console.log('tied game both pick scissors'); // both pick scissors
-} else if (bot1 === choices[1] &  bot2 === choices[0]){
+} else if (bot1 === choices[1] &&  bot2 === choices[0]){
     console.log('paper beats rock');  
     game.win += 1;
-    result += 1
-} else if(bot1 === choices[2] &  bot2 === choices[1]){
+} else if(bot1 === choices[2] && bot2 === choices[1]){
     console.log('scissors beats paper');
-}else if (bot1 === choices[0] &  bot2 === choices[2]){
+}else if (bot1 === choices[0] && bot2 === choices[2]){
     console.log('rock beats scissors')  
 } 
 
 
 if(game.win > game.lose){
     result = bot1 + " beats " + bot2;
+    game.win = game.win += 1;
 }else if(game.win === game.lose){
     console.log('tied game');
 } else if (game.lose > game.win) {
     result += bot2 + ' beats ' + bot1;
+    game.lose = game.lose += 1;
 } else {
     console.log('error');
 }
 console.log(result);
+console.log(game);

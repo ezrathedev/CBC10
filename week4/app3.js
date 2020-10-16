b3c2ad311b6010e4f02310624356e6c6688daaef
unknown <anin4325@gmail.com>
2020-10-15 20:08:09 -0700

    game fixed

--- week4/app3.js
+++ week4/app3.js
@@ -26,21 +26,31 @@ if (bot1 === choices[0] & bot2 === choices[0]){
     game.win += 1;
 } else if(bot1 === choices[2] && bot2 === choices[1]){
     console.log('scissors beats paper');
+    game.win = game.win += 1;
 }else if (bot1 === choices[0] && bot2 === choices[2]){
     console.log('rock beats scissors')  
+}  else if (bot2 === choices[1] &&  bot1 === choices[0]){
+    console.log('paper beats rock');  
+    game.lose += 1;
+} else if(bot2 === choices[2] && bot1 === choices[1]){
+    console.log('scissors beats paper');
+    game.lose = game.lose += 1;
+}else if (bot2 === choices[0] && bot1 === choices[2]){
+    console.log('rock beats scissors')  
+    game.lose = game.lose += 1;
 } 
 
 
 if(game.win > game.lose){
-    result = bot1 + " beats " + bot2;
-    game.win = game.win += 1;
+    result += bot1 + " beats " + bot2;
+    console.log(result);
+   
 }else if(game.win === game.lose){
     console.log('tied game');
 } else if (game.lose > game.win) {
-    result += bot2 + ' beats ' + bot1;
-    game.lose = game.lose += 1;
+    result = result += bot2 + ' beats ' + bot1;
+    console.log(result);
 } else {
     console.log('error');
 }
-console.log(result);
 console.log(game);

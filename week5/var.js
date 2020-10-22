788152db5a74c3d49afb679ecca1260e626a114a
Ezra Hampton <ezrahampton@Ezras-MacBook-Pro.local>
2020-10-22 08:57:06 -0400

    working on homework

--- week5/var.js
+++ week5/var.js
@@ -1,10 +1,34 @@
-// const foo = () => {
-//     document.getElementById("#foo").style.color="green";
-// }
 
-var user = document.getElementById('username');
+var username = document.getElementById('username');
+var user = document.getElementById('user');
 var password = document.getElementById('password');
-var submit = document.getElementById('submit');
+var submit = document.getElementById('input[type=submit]');
+var  userLabel = ''; //line 6
+var passlabel = ''; // line 7
+var rememberMe = document.querySelector('input[type=checkbox]');
+var submitBtn = document.getElementById('submit'); // line 9
 
+username.addEventListener('focus', function(){
+    //do something here!  line 10
+});
+
+username.addEventListener('blur', function(){
+    // somesomething  line 11
+})
+password.addEventListener('focus', function(){
+    //do something here!  line 12
+});
+
+password.addEventListener('blur', function(){
+    // somesomething  line 13
+});
+
+rememberMe.addEventListener('click', function(){
+    // line 13 use if statement..
+});
+
+submitBtn.addEventListener('click', function(){
+// line 13
+});
 
 

95cc69a52523682d3dde4a07ad4edf860e5b80d9
Ezra Hampton <ezrahampton@Ezras-MacBook-Pro.local>
2020-10-22 13:50:06 -0400

    message

--- week5/scripts.js
+++ week5/scripts.js
@@ -3,28 +3,30 @@ var username = document.getElementById('username');
 var user = document.getElementById('user');
 var password = document.getElementById('password');
 var submit = document.getElementById('input[type=submit]');
-var  userLabel = ''; //line 6  // use .value ?
-var passlabel = ''; // line 7
+var  userLabel = document.querySelector('label[for=username]'); //line 6  // use label[for=username]
+var passlabel = document.querySelector('label[for=password]'); 
 var rememberMe = document.querySelector('input[type=checkbox]');
 var submitBtn = document.getElementById('submit'); // line 9
 
-username.addEventListener('focus', function(){
-    //do something here!  line 10
+username.addEventListener('focus', function(e){
+    e.target(username);
 });
 
 username.addEventListener('blur', function(){
-    // somesomething  line 11
+    //  // use classlist .remove to remove the styles from the input
 })
 password.addEventListener('focus', function(){
-    //do something here!  line 12
+    // use classlist to add style to the input box
 });
 
 password.addEventListener('blur', function(){
-    // somesomething  line 13
+    // use classlist .remove to remove the styles from the input
+    Element.classlist.remove('');
 });
 
 rememberMe.addEventListener('click', function(){
-    // line 13 use if statement..
+    // use classlist .add to add style to the input box !
+    Element.classlist.add('');
 });
 
 submitBtn.addEventListener('click', function(){

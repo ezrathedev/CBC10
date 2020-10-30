431a710e4620d6c480f7038222bb2b783aaf316d
unknown <anin4325@gmail.com>
2020-10-29 17:01:21 -0700

    working on homework

--- week6/review.js
+++ week6/review.js
@@ -3,29 +3,28 @@ let var1;
 
 // 2) assign a value to var1 of to the string of 'string'
 var1 = 'string';
-// console.log(var1);
 // 3) declare and assign a new variable named var2 to the "g" from var1 using square bracket notation
 let var2 = var1.indexOf('g');
-// console.log(var2);
+console.log(var2);
 // 4) append the string text "o!" onto var2 
 var2 += 'o!';
-// console.log(var2); //! come back to this!!
+console.log(var2); //! come back to this!!
 // 5) create a variable named var3 assign it to any value of type number
-let var3 = 000000;
+let var3 = 0;
 // 6) create a variable named var4 assign it to a string type the value should be a number character. Example: var var4 = "5";
 let var4 = '3';
 // 7) write the result of var3 + var4 to the console or the document. You should see the concatenation of var3 + var4
-// console.log(var3 + var4);
+console.log(var3 + var4);
 // 8) Write to the console or document using parseFloat || parseInt (depending on whether you are dealing with decimal or an integer) the sum of var3 and var4. Example: CORRECT: 5 + 5 = 10 INCORRECT: 5 + 5 = 55 basically not concatenation 
-// console.log(parseInt(var4));
+console.log(parseInt(var4));
 // 9) Create a variable named arr with the value of an empty array
 let arr = [];
 // 10) write a for loop that adds 1 - 10 onto the array. The resutling array should be arr = [1,2,3...9, 10]; HINT: you'll start at 1 instead of 0 inside the code block of the for loop we want to use the push method, you can use anything you like the that creates the desired end result;
 for(var i =1; i <= 10; i++){
     // arr[i] = i;
-    arr.push(i);
+    arr[i] = i;
 }
-// console.log(arr);
+console.log(arr);
 // 11) Add 11 on to the end of our array with an array method. result should look like this arr = [1,2,3,...9,10,11] HINT: push
 arr.push(11);
 console.log(arr);
@@ -39,15 +38,13 @@ console.log(arr);
 arr.shift(); // should remove the zero from the begining of the array
 console.log(arr);
 // 15) use splice to remove the first element of our array. resutls should be arr = [2,3..9,10] HINT: takes three parameters the first is starting index, the second is how many elements, all the following parameters are things to add in after the elements specified to be removed are removed, if any are. 
-arr.splice(0,1); // this removed 1 this is what i wanted.
+arr.splice(0,1); // removed an empty slot ?? should remove 1
 console.log(arr);
 // 16) use splice to remove the last two elements of the array. results should be arr = [2,3..7,8] HINT: the index of the next to last element would be ()arr.length - 2)
-arr.splice(arr.length -2,9);
-console.log(arr);!
+arr.splice(arr.length -1, 10 ,8 )
+console.log(arr);// this should be right.
 // 17) use splice to add -1, 0, 1 onto the front of our array. results should be arr = [-1,0,1,2,3...7,8];
-arr.splice(0, 0,-1,0,1); // this isnt right it puts the things o
-arr.splice(arr.length)
-console.log(arr);
+arr.splice(); // double check this one .
 // 18) use slice to make a new array called copy, it should have the first 3 elements in copy. results should look like arr = [-1,0,1,2,3...7,8] (unchanged) and copy = [-1,0,1]
-let copy = arr.slice(0, 3);
+let copy = arr.slice(0, 4);
 console.log(copy);

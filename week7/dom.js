fc3d6e46b3a007a95ddd969f4e1e9e1262819e4c
unknown <anin4325@gmail.com>
2020-11-03 18:17:55 -0800

    working on homework

---
+++ week7/dom.js
@@ -0,0 +1,12 @@
+let heading = document.querySelector('div');
+let paragraph = document.querySelector('p');
+let ul = document.querySelector('ul');
+let hobbi ;
+
+const hobbies = ['coding', 'running', 'soccer', 'video editing'];
+
+for(let i = 0; i < hobbies.length; i++) {
+    hobbi = hobbies[i];
+}
+console.log(hobbi);
+

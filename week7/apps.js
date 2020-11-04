d49a739940b0da68b69a33842edc1c497c2fd0e6
unknown <anin4325@gmail.com>
2020-11-03 19:27:10 -0800

    message

---
+++ week7/apps.js
@@ -0,0 +1,9 @@
+let heading = document.querySelector('div h1');// step 1
+let 
+
+
+// use a for each loop 
+
+// step 6
+const hobbies = ['soccer', 'coding', 'guitar', 'running', 'cycling']//step 6
+console.log(hobbies);

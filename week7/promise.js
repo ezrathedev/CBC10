dd7347ccae5386064a3d79362f755040348d2f37
Ezra Hampton <ezrahampton@Ezras-MacBook-Pro.local>
2020-11-05 16:07:57 -0500

    homework done

--- week7/promise.js
+++ week7/promise.js
@@ -1,13 +1,11 @@
 const promise1 = () => {
     return new Promise((resolve, reject) => {
-        resolve();
+        resolve('i have resolved');
     })
 }
-promise1();
-promise1.then(() => {
-    console.log('success')
-});
-promise1.catch(() => {
+promise1().then(() => {
+    console.log('Success');
+}).catch(() => {
     console.log('there was an error');
 });
 
@@ -26,3 +24,39 @@ promise2(true)
 promise2(false)
     .then(data => {console.log(data)})
     .catch(err => {console.log(err)})
+
+const promise3 = new Promise((resolve, reject) => {
+    resolve('promise3 is complete');
+});
+
+const promise4 = new Promise((resolve, reject) => {
+    resolve('promise4 is complete');
+})
+promise4.then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err);
+});
+
+const promise5 = new Promise((resolve, reject) => {
+
+})
+Promise.all([promise3,promise4,promise5])
+    .then(data => { console.log(data) })
+    .catch(err => { console.log(err) })
+
+
+let ul = document.querySelector('ul');
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://api.icndb.com/jokes/random?limitTo=[nerdy]', true);
+
+xhr.onload = function () {
+	console.log(this.responseText);
+	const joke = JSON.parse(this.responseText).value.joke;
+	console.log(joke);
+	ul.innerHTML = `<li>${joke}</li>`;
+};
+
+xhr.send();

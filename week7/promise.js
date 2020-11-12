048fde9e090638fc389c38492a920f92e2704c6b
HBull5 <harcourtbullv@gmail.com>
2020-11-11 21:03:00 -0500

    feedback complete

--- week7/promise.js
+++ week7/promise.js
@@ -1,56 +1,78 @@
 const promise1 = () => {
-    return new Promise((resolve, reject) => {
-        resolve('i have resolved');
-    })
-}
-promise1().then(() => {
-    console.log('Success');
-}).catch(() => {
-    console.log('there was an error');
-});
+	return new Promise((resolve, reject) => {
+		resolve('i have resolved');
+	});
+};
+
+promise1()
+	.then(() => {
+		console.log('Success');
+	})
+	.catch(() => {
+		console.log('there was an error');
+	});
 
 const promise2 = (errorboolean) => {
-    return new Promise((resolve, reject) => {
-        if(errorboolean) {
-            reject('there was an error');
-        } else {
-            resolve('your data you asked for');
-        }
-    })
-}
+	return new Promise((resolve, reject) => {
+		if (errorboolean) {
+			reject('there was an error');
+		} else {
+			resolve('your data you asked for');
+		}
+	});
+};
+
 promise2(true)
-    .then(data => {console.log(data)})
-    .catch(err => {console.log(err)})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
 promise2(false)
-    .then(data => {console.log(data)})
-    .catch(err => {console.log(err)})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 const promise3 = new Promise((resolve, reject) => {
-    resolve('promise3 is complete');
+	resolve('promise3 is complete');
 });
 
 const promise4 = new Promise((resolve, reject) => {
-    resolve('promise4 is complete');
-})
-promise4.then(data => {
-    console.log(data)
-}).catch(err => {
-    console.log(err);
+	resolve('promise4 is complete'); //? modified for step 16
 });
 
+promise4
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+// const promise5 = new Promise((resolve, reject) => {}); //? This isn't complete the promise should resolve. This is neither resolving or rejecting
 const promise5 = new Promise((resolve, reject) => {
+	resolve('Promise 5 is complete');
+});
 
-})
-Promise.all([promise3,promise4,promise5])
-    .then(data => { console.log(data) })
-    .catch(err => { console.log(err) })
-
+Promise.all([promise3, promise4, promise5])
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 let ul = document.querySelector('ul');
 
 let xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://api.icndb.com/jokes/random?limitTo=[nerdy]', true);
+// xhr.open('GET', 'http://api.icndb.com/jokes/random?limitTo=[nerdy]', true); //? This was supposed to get a specific joke. It was supposed to get joke with the id of 15. That joke is "When Chuck Norris goes to donate blood, he declines the syringe, and instead requests a hand gun and a bucket." read about it here --> http://www.icndb.com/api/ SECTION: "Fetching a specific joke"
+xhr.open('GET', 'http://api.icndb.com/jokes/15', true);
 
 xhr.onload = function () {
 	console.log(this.responseText);
@@ -60,3 +82,31 @@ xhr.onload = function () {
 };
 
 xhr.send();
+
+//? HTML + JS to fetch a certain number of jokes then populate a ul with them
+const input = document.querySelector('input[type=number]');
+const output = document.querySelector('#jokes');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+	let numJokes = parseInt(input.value); //? We can't get 2.5 jokes so parsing to an int instead of a float
+
+	if (isNaN(numJokes)) {
+		numJokes = 1;
+	}
+
+	let ajax = new XMLHttpRequest();
+
+	ajax.open('GET', `http://api.icndb.com/jokes/random/${numJokes}`, true);
+
+	ajax.onload = function () {
+		const jokes = JSON.parse(this.responseText).value;
+
+		output.innerHTML = '';
+		for (const joke of jokes) {
+			output.innerHTML += `<li>${joke.joke}</li>`;
+		}
+	};
+
+	ajax.send();
+});

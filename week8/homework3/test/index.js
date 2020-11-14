c9d17049a8b0d2ad2d329aacc42d23be80baf0bb
Ezra Hampton <ezrahampton@Ezras-MacBook-Pro.local>
2020-11-14 15:30:43 -0500

    done

---
+++ week8/homework3/test/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true })); // used to get the form data.
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views/login.html'), (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
+
+app.get('/:page', (req, res) => {
+    
+    if (req.params.page == 1 ) {
+        res.sendFile(path.join(__dirname, `views/page_1.html`))
+    
+    } else if (req.params.page == 2 ) {
+        res.sendFile(path.join(__dirname, `views/page_2.html`))
+
+    } else if (req.params.page == 3 ) {
+        res.sendFile(path.join(__dirname, `views/page_3.html`))
+    }    
+});
+
+app.post('/', (req,res)=> {
+    res.end(`<h1>This is ${req.body.username}'s page</h1>`);
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}...`);
+});
--- week8/homework3/test/test.js
+++ week8/homework3/test/test.js
@@ -1,1 +1,21 @@
-const foo = require('../foo')
+// importing my function from another file
+const foo = require('../foo');
+const assert = require('assert');
+
+describe('foo should return... ', () => {
+    it('it should return bar', () => {
+        assert.strictEqual(foo(), "bar")})
+});
+
+describe('foo should return... ', () => {
+    it('it should return bar', () => {
+        assert.strictEqual(foo(), 'bar')
+    })
+})
+
+describe('foo should return..', () => {
+    it('it should return bat', () => {
+        assert.strictEqual(foo(), 'bat')
+    })
+})
+

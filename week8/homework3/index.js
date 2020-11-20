b6e56bd43afd01a9dda62c80b57bedd86ade7b8e
kevinfair1 <kevinfairaso@gmail.com>
2020-11-20 13:12:00 -0500

    wk8_hw3 feedback

--- week8/homework3/index.js
+++ week8/homework3/index.js
@@ -1,37 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true })); // used to get the form data.
 
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'views/login.html'), (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
 
-app.get('/page', (req, res) => {
-    
-    if (req.params.page == 1 ) {
-        res.sendFile(path.join(__dirname, `views/page_1.html`))
-    
-    } else if (req.params.page == 2 ) {
-        res.sendFile(path.join(__dirname, `views/page_2.html`))
+//! Step 15 - Your code here is correct, but does not work because you did not import
+//! the view files (html files) which were given to you in the assignment. 3/4 credit
 
-    } else if (req.params.page == 3 ) {
-        res.sendFile(path.join(__dirname, `views/page_3.html`))
-    }    
+app.get("/page", (req, res) => {
+  if (req.params.page == 1) {
+    res.sendFile(path.join(__dirname, `views/page_1.html`));
+  } else if (req.params.page == 2) {
+    res.sendFile(path.join(__dirname, `views/page_2.html`));
+  } else if (req.params.page == 3) {
+    res.sendFile(path.join(__dirname, `views/page_3.html`));
+  }
 });
 
-app.post('/', (req,res)=> {
-    res.end(`<h1>This is ${req.body.username}'s page</h1>`);
-})
+//! You are missing the colon on line 20, which comes after the slash in '/page', should be
+//! '/:page'. Other then that, your code looks great. However, it does not work because
+//! you did not import the require view files (html pages). 3/4 credit
+
+app.post("/", (req, res) => {
+  res.end(`<h1>This is ${req.body.username}'s page</h1>`);
+});
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port ${PORT}...`);
 });
 
+//! Great code again here, but not working without view files. 3/4 pt
--- week8/homework3/test/test.js
+++ week8/homework3/test/test.js
@@ -1,21 +1,27 @@
 // importing my function from another file
-const foo = require('../foo');
-const assert = require('assert');
+const foo = require("../foo");
+const assert = require("assert");
 
-describe('foo should return... ', () => {
-    it('it should return bar', () => {
-        assert.strictEqual(foo(), "bar")})
+describe("foo should return... ", () => {
+  it("it should return bar", () => {
+    assert.strictEqual(foo(), "bar");
+  });
 });
 
-describe('foo should return... ', () => {
-    it('it should return bar', () => {
-        assert.strictEqual(foo(), 'bar')
-    })
-})
+describe("foo should return... ", () => {
+  it("it should return bar", () => {
+    assert.strictEqual(foo(), "bar");
+  });
+});
 
-describe('foo should return..', () => {
-    it('it should return bat', () => {
-        assert.strictEqual(foo(), 'bat')
-    })
-})
+describe("foo should return..", () => {
+  it("it should return bat", () => {
+    assert.strictEqual(foo(), "bat");
+  });
+});
 
+//! Your test is not working here because you created another package.json file without
+//! mocha installed or declared in the scripts-> test: "mocha". Installing the modules in the
+//! root directory would have been enough If you do want to install the package.json here, you
+//! can, but you didn't install mocha, nor did you set the scripts to test: "mocha" in the json
+//! file.
